b2ea81fc5ffd9d4e2fdd1c88580b7aaeceea8db8
Felipe Faria <2829082+synchronizing@users.noreply.github.com>
2021-01-06 00:54:28 -0500

    Removed conf Google string hack, and modified autoclass_content.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ autodoc_typehints = "none"
 add_module_names = False
 
 # Show only class docs.
-autoclass_content = "class"
+autoclass_content = "both"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -72,41 +72,3 @@ html_theme = "furo"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-
-# https://michaelgoerz.net/notes/extending-sphinx-napoleon-docstring-sections.html
-# -- Extensions to the  Napoleon GoogleDocstring class ---------------------
-
-from sphinx.ext.napoleon.docstring import GoogleDocstring
-
-# first, we define new methods for any new sections and add them to the class
-def parse_keys_section(self, section):
-    return self._format_fields("Keys", self._consume_fields())
-
-
-GoogleDocstring._parse_keys_section = parse_keys_section
-
-
-def parse_attributes_section(self, section):
-    return self._format_fields("Attributes", self._consume_fields())
-
-
-GoogleDocstring._parse_attributes_section = parse_attributes_section
-
-
-def parse_class_attributes_section(self, section):
-    return self._format_fields("Class Attributes", self._consume_fields())
-
-
-GoogleDocstring._parse_class_attributes_section = parse_class_attributes_section
-
-# we now patch the parse method to guarantee that the the above methods are
-# assigned to the _section dict
-def patched_parse(self):
-    self._sections["keys"] = self._parse_keys_section
-    self._sections["class attributes"] = self._parse_class_attributes_section
-    self._unpatched_parse()
-
-
-GoogleDocstring._unpatched_parse = GoogleDocstring._parse
-GoogleDocstring._parse = patched_parse
