f89009149d6862611b276ce6a7e93f9ff34f5c70
Felipe Faria <2829082+synchronizing@users.noreply.github.com>
2021-04-01 01:23:33 -0400

    Improved package search function. (#9)

--- toolbox/pkgutil/package.py
+++ toolbox/pkgutil/package.py
@@ -1,8 +1,8 @@
 import importlib
-import pkgutil
+import pkg_resources
 
 
-def search_package(pkg: str, method: str = "is") -> dict:
+def search_package(pkg: str, method: str = "is", imports: bool = False) -> dict:
     """Discover packages installed in the system.
 
     Function iterates through all of the installed packages in the system and returns a dictionary
@@ -11,6 +11,7 @@ def search_package(pkg: str, method: str = "is") -> dict:
     Args:
         pkg: Package name to search for.
         method: String that is either ``is``, ``in``, or ``startswith``.
+        imports: Boolean that indicates whether or not to import the found package(s).
 
     Raises:
         TypeError: The search method passed is invalid.
@@ -22,6 +23,14 @@ def search_package(pkg: str, method: str = "is") -> dict:
         * ``in``: Returns modules that contains the string ``pkg``.
         * ``startswith``: Return modules that starts with the passed ``pkg`` name.
 
+        When ``imports`` is set to ``False`` this function returns a
+        ``Dict[str, str]`` where the key is the name of the module, and
+        the value is the version installed on the system.
+
+        If ``imports`` is set to ``True`` this function returns a
+        ``Dict[str, Module]`` where the key is the name of the module,
+        and the value is the _imported_ module.
+
     Example:
 
         .. code-block:: python
@@ -29,7 +38,11 @@ def search_package(pkg: str, method: str = "is") -> dict:
             from toolbox.pkgutil.package import search_package
 
             print(search_package("toolbox", method="is"))
-            # >>> {'toolbox': <module 'toolbox' from '.../toolbox/__init__.py'>}
+            # >>> {'toolbox': '1.4.0'}
+
+            print(search_package("toolbox", method="is", imports=True))
+            # >>> {'toolbox': <module 'toolbox' from '.../toolbox/toolbox/__init__.py'>}
+
     """
 
     if method == "is":
@@ -42,9 +55,18 @@ def search_package(pkg: str, method: str = "is") -> dict:
         err = "Search method must be either 'is', 'in', or 'startswith'."
         raise TypeError(err)
 
+    print(method)
+
+    installed_pkgs = {
+        pkg.project_name: pkg.version for pkg in pkg_resources.working_set
+    }
+
     packages = {}
-    for _, name, ispkg in pkgutil.iter_modules():
-        if ispkg and search(name):
-            packages[name] = importlib.import_module(name)
+    for package, version in installed_pkgs.items():
+        if search(package):
+            if imports:
+                packages[package] = importlib.import_module(package)
+            else:
+                packages[package] = version
 
     return packages
